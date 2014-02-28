62eb494fa4c57c724955ee8810724a561be755a7
zhanggaofeng <zhanggaofeng@wmfly.net>
2014-02-28 18:42:59 +0800

    dddddddddddddd

--- SpringMina/src/com/num/config/Configs.java
+++ SpringMina/src/com/num/config/Configs.java
@@ -13,4 +13,6 @@ public class Configs {
     public static final int game_idle_both_time = 10;
     public static final int game_write_timeout = 10;
     public static final boolean game_session_read_oper = true;
+    public static final int game_core_pool_size = 200;
+    public static final int game_max_pool_size = 250;
 }
