0bd18df734c2b1cb012ec327fa78a2f55f8ee292
zhanggaofeng <zhanggaofeng@wmfly.net>
2014-02-28 11:44:31 +0800

    mina decoder and encoder

--- SpringMina/src/com/num/mina/codec/GameProtoEncoder.java
+++ SpringMina/src/com/num/mina/codec/GameProtoEncoder.java
@@ -4,7 +4,9 @@
  */
 package com.num.mina.codec;
 
+import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import org.apache.mina.core.buffer.IoBuffer;
 import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolEncoderAdapter;
 import org.apache.mina.filter.codec.ProtocolEncoderOutput;
@@ -16,6 +18,7 @@ import org.apache.mina.filter.codec.ProtocolEncoderOutput;
 public class GameProtoEncoder extends ProtocolEncoderAdapter{
 
     private Charset charset;
+    private static final int data_size = 4;// 数据长度值字节数
 
     public GameProtoEncoder() {
         charset = Charset.forName("utf8");
@@ -27,6 +30,14 @@ public class GameProtoEncoder extends ProtocolEncoderAdapter{
 
     @Override
     public void encode(IoSession is, Object o, ProtocolEncoderOutput peo) throws Exception {
-        
+        this.encode(is, (ByteBuffer)o, peo);
+    }
+    
+    private void encode(IoSession session, ByteBuffer data, ProtocolEncoderOutput out) {
+        int dataLen = data.array().length + data_size;
+        IoBuffer buf = IoBuffer.allocate(dataLen).setAutoExpand(true);
+        buf.putInt(dataLen);
+        buf.put(data);
+        out.write(buf);
     }
 }
