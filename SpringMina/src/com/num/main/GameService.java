8167e74172c9e6f332fe8c241f1cb2a7056fffd3
zhanggaofeng1 <zhanggaofeng1@gmail.com>
2014-03-03 21:47:22 +0800

    log4j config commit

--- SpringMina/src/com/num/main/GameService.java
+++ SpringMina/src/com/num/main/GameService.java
@@ -9,6 +9,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.num.config.Configs;
 import com.num.main.service.ShutdownService;
 import com.num.main.service.StartService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -17,12 +26,26 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class GameService {
 
+     private static final org.slf4j.Logger log = LoggerFactory.getLogger(StartService.class);
     @Autowired
     private StartService startService;
     @Autowired
     private ShutdownService shutdownService;
 
+    static {
+        // 启动载入log4j配置文件
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream(Configs.log4j_cfg_path));
+            PropertyConfigurator.configure(props);
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+        
+    }
+
     public static void main(String... args) {
+
         ApplicationContext context = new ClassPathXmlApplicationContext(Configs.spring_cfg_path);
         GameService gameService = context.getBean(GameService.class);
         gameService.startGame();
@@ -33,7 +56,7 @@ public class GameService {
     public void startGame() {
         startService.startGame();
     }
-    
+
     // 关闭服务器调用
     public void shutdownGame() {
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
@@ -43,5 +66,4 @@ public class GameService {
             }
         }));
     }
-    
 }
