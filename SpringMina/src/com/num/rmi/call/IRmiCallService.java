b2232a5bdeb132cc9b64fb49de904930e22f3f8c
zhanggaofeng1 <zhanggaofeng1@gmail.com>
2014-03-04 23:27:23 +0800

    add rmi config and class

---
+++ SpringMina/src/com/num/rmi/call/IRmiCallService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.num.rmi.call;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface IRmiCallService {
+    
+}
---
+++ SpringMina/src/com/num/rmi/impl/RemoteRmiService.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.num.rmi.impl;
+
+import com.num.rmi.IRmiService;
+
+/**
+ *
+ * @author Administrator
+ */
+public class RemoteRmiService implements IRmiService{
+    
+}
